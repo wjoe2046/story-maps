6808cd82c3803e92ca746b67ff023a337ae2f91b
Wenfei Zhou <wzhou2046@gmail.com>
2020-06-08 13:10:20 -0400

    added the map to the page, below the nav bar

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
@@ -35,8 +36,32 @@ const styles = (themes) => ({
   },
 });
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13,
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewPort] = useState(INITIAL_VIEWPORT);
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width='100vw'
+        height='calc(100vh - 64px)'
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+        mapboxApiAccessToken='pk.eyJ1Ijoid2pvZTIwNDYiLCJhIjoiY2s4am9iaG5uMGowczNncWlvampneGo1ZCJ9.WpK_4GDmx_1UIjRdgJeGoQ'
+        onViewportChange={(newViewport) => setViewPort(newViewport)}
+        {...viewport}
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={(newViewport) => setViewPort(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 export default withStyles(styles)(Map);
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Map from '../components/Map';
 import withRoot from '../withRoot';
 
 const App = () => {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Map />
+    </React.Fragment>
+  );
 };
 
 export default withRoot(App);
